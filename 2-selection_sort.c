37a5844ebbc76ec872b39211bbb9ed71b73a58fb
Phoeb2 <ojoloanu@gmail.com>
2024-07-19 06:52:34 -0700

    Sortalg

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,45 +1,34 @@
-#include "sort.h" 
-  
- /** 
-  * swap_ints - Swap two integers in an array. 
-  * @a: The first integer to swap. 
-  * @b: The second integer to swap. 
-  */ 
- void swap_ints(int *a, int *b) 
- { 
-         int tmp; 
-  
-         tmp = *a; 
-         *a = *b; 
-         *b = tmp; 
- } 
-  
- /** 
-  * selection_sort - Sort an array of integers in ascending order 
-  *                  using the selection sort algorithm. 
-  * @array: An array of integers. 
-  * @size: The size of the array. 
-  * 
-  * Description: Prints the array after each swap. 
-  */ 
- void selection_sort(int *array, size_t size) 
- { 
-         int *min; 
-         size_t i, j; 
-  
-         if (array == NULL || size < 2) 
-                 return; 
-  
-         for (i = 0; i < size - 1; i++) 
-         { 
-                 min = array + i; 
-                 for (j = i + 1; j < size; j++) 
-                         min = (array[j] < *min) ? (array + j) : min; 
-  
-                 if ((array + i) != min) 
-                 { 
-                         swap_ints(array + i, min); 
-                         print_array(array, size); 
-                 } 
-         } 
- }
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: The array
+ * @size: The size of the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, step, min_idx;
+	int tmp;
+
+	for (step = 0; step < size - 1; step++)
+	{
+		min_idx = step;
+		for (i = step + 1; i < size; i++)
+		{
+			if (array[i] < array[min_idx])
+				min_idx = i;
+		}
+
+		if (min_idx != step)
+		{
+			tmp = array[step];
+			array[step] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
+}
